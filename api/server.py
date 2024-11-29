b75ec6bb224df86287eadba9b421199d4bf120b6
Brian Aji Pamungkas <brianajiks123@gmail.com>
2024-11-29 22:58:32 +0700

    add features: simple crud for product

--- api/server.py
+++ api/server.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from tortoise.contrib.fastapi import register_tortoise
-from db.models import Supplier, supplier_pydantic, supplier_pydanticIn
+from db.models import Supplier, supplier_pydantic, supplier_pydanticIn, Product, product_pydantic, product_pydanticIn
 
 app = FastAPI()
 
@@ -12,10 +12,12 @@ register_tortoise(
     add_exception_handlers=True
 )
 
+# Welcome Route
 @app.get("/")
 def index() -> dict:
     return {"message": "Hello Broo... You can see API Spec, let's go to /docs or /redoc"}
 
+# Supplier Routes
 @app.post("/supplier")
 async def add_supplier(supplier_info: supplier_pydanticIn) -> dict: # type: ignore
     supplier_obj = await Supplier.create(**supplier_info.dict(exclude_unset=True))
@@ -53,4 +55,49 @@ async def delete_supplier(supplier_id: int) -> dict:
     
     await supplier.delete()
     
-    return {"message": "Success to deleting the data."}
+    return {"message": "Success to deleting the supplier."}
+
+# Product Routes
+@app.post("/product/{supplier_id}")
+async def add_product(supplier_id: int, product_details: product_pydanticIn) -> dict: # type: ignore
+    supplier = await Supplier.get(id=supplier_id)
+    product_details = product_details.dict(exclude_unset=True)
+    
+    product_details["revenue"] += product_details["quantity_sold"] * product_details["unit_price"]
+    
+    product_obj = await Product.create(**product_details, supplied_by=supplier)
+    
+    response = await product_pydantic.from_tortoise_orm(product_obj)
+    return {"data": response}
+
+@app.get("/product")
+async def get_all_products() -> dict:
+    response = await product_pydantic.from_queryset(Product.all())
+    return {"data": response}
+
+@app.get("/product/{product_id}")
+async def get_product(product_id: int) -> dict:
+    response = await product_pydantic.from_queryset_single(Product.get(id=product_id))
+    return {"data": response}
+
+@app.put("/product/{product_id}")
+async def update_product(product_id: int, update_info: product_pydanticIn) -> dict: # type: ignore
+    product = await Product.get(id=product_id)
+    update_info = update_info.dict(exclude_unset=True)
+    
+    product.name = update_info["name"]
+    product.quantity_in_stock = update_info["quantity_in_stock"]
+    product.quantity_sold += update_info["quantity_sold"]
+    product.unit_price = update_info["unit_price"]
+    product.revenue += (update_info["quantity_sold"] * update_info["unit_price"]) + update_info["revenue"]
+    
+    await product.save()
+    
+    response = await product_pydantic.from_tortoise_orm(product)
+    return {"data": response}
+
+@app.delete("/product/{product_id}")
+async def delete_product(product_id: int) -> dict:
+    await Product.filter(id=product_id).delete()
+    
+    return {"message": "Success to deleting the product."}
